5fd3dd8cbdfca2a3d1ded98297fa1a58284540cc
Jakob Aarøe Dam <jakob.a.dam@gmail.com>
2011-05-19 11:48:17 +0200

    glg for git log instead of gll

--- aliases/git.aliases.bash
+++ aliases/git.aliases.bash
@@ -25,7 +25,7 @@ alias gco='git checkout'
 alias gexport='git archive --format zip --output'
 alias gdel='git branch -D'
 alias gmu='git fetch origin -v; git fetch upstream -v; git merge upstream/master'
-alias gll='git log --graph --pretty=oneline --abbrev-commit'
+alias glg='git log --graph --pretty=oneline --abbrev-commit'
 
 case $OSTYPE in
   linux*)
